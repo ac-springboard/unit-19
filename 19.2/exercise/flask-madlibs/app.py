b91c97410f0d7c8688c197bca3bcc1492c6590c5
Almir Campos <almircampos@gmail.com>
2020-11-17 03:03:00 -0800

    [U1902] Exercise - Form completed (with some styling)

--- 19.2/exercise/flask-madlibs/app.py
+++ 19.2/exercise/flask-madlibs/app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from json import dumps
+from flask import Flask, render_template
 import stories
 
 app = Flask(__name__)
 
-ans = {
-    'place': 'Andromeda',
-    'adjective': 'interesting',
-    'noun': 'singularity',
-    'verb': 'open and close',
-    'plural_noun': 'worm holes'
+default_params = {
+    "place": "Andromeda",
+    "adjective": "interesting",
+    "noun": "singularity",
+    "verb": "open and close",
+    "plural_noun": "worm holes"
 }
-story = stories.story_structure.generate(ans)
-print(story)
+
+
+# default_story = stories.story_structure.generate(default_params)
+
+@app.route('/')
+def r_root():
+    print(dumps(default_params))
+    return render_template("root.html", par=default_params)
