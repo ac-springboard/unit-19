bc00a4d295d7f4a4ddcc506c4af635e6e232b298
Almir Campos <almircampos@gmail.com>
2020-11-19 09:52:13 -0800

    [U1903] Exercise Completed - v2.0 - Question number controlled on the server side.

--- 19.3/exercise/flask-survey/app.py
+++ 19.3/exercise/flask-survey/app.py
@@ -19,6 +19,7 @@ thankyou_statement = "Thank you!"
 
 @app.route('/')
 def root_view():
+    responses.clear();
     return render_template('root.html',
                            survey_title=survey.title,
                            survey_instructions=survey.instructions,
@@ -26,31 +27,29 @@ def root_view():
                            page_title='Survey Home')
 
 
-@app.route('/questions/<int:question_number>')
-def questions_view(question_number):
-    if is_invalid_request(question_number):
-        return redirect('/')
-
-    if is_last_question(question_number):
+@app.route('/questions')
+def questions_view():
+    if is_this_last_question():
         congrats = """
         Congratulations! You made it to the last question of this survey!
-        When you answer to this question you will be automatically redirect to you Thank You page where a gift is waiting for you!
+        When you answer to this question you will be automatically redirected to our Thank You page!
         """
     else:
         congrats = None
+
+    question_number = get_this_question_number() - 1
     return render_template('questions.html',
                            survey_title=survey.title,
-                           question_statement=f"{question_number}. {survey.questions[question_number].question}",
+                           question_statement=f"{question_number + 1}. {survey.questions[question_number].question}",
                            question_choices=survey.questions[question_number].choices,
-                           question_number=question_number,
                            top_message=congrats,
                            page_label='questions_page',
-                           page_title=f"Question #{question_number}")
+                           page_title=f"Question #{question_number + 1}")
 
 
-@app.route("/answer/<int:question_number>", methods=['GET'])
-def answer_service(question_number):
-    if is_invalid_request(question_number):
+@app.route("/answer", methods=['GET'])
+def answer_service():
+    if is_invalid_request():
         return redirect('/')
 
     try:
@@ -59,41 +58,53 @@ def answer_service(question_number):
         else:
             answer = request.form['answer']
         responses.append(answer)
+        print(answer)
+        print(responses)
     except Exception as e:
         flash('Something seems to be wrong. Have you answered the question?', ['questions_page', 'error'])
         flash("""If you have answered the question, 
         please contact Joe Biden and Kamala Harris
         at the White House on Jan 20, 2021 to file your complaint.""")
-        return redirect(f'/questions/{question_number}')
+        return redirect('/questions')
 
-    if is_last_question(question_number):
+    if is_last_answered_question():
         return redirect('/thankyou')
 
-    return redirect(f'/questions/{len(responses)}')
+    return redirect('/questions')
 
 
 @app.route("/thankyou")
 def thankyou_view():
-    responses.clear();
+    responses.clear()
     return render_template('thankyou.html',
+                           survey_title=survey.title,
                            thankyou_statement=thankyou_statement,
                            page_label='thankyou_page')
 
 
 # AUXILIARY FUNCTIONS
 
-def is_last_question(question_number):
-    return question_number == len(survey.questions) - 1
+def is_last_answered_question():
+    it_is = get_question_number() == len(survey.questions)
+    return it_is
 
 
-def is_invalid_request(question_number):
-    return is_survey_closed() or is_invalid_question_number(question_number)
+def is_invalid_request():
+    return is_survey_closed()
 
 
 def is_survey_closed():
     return len(responses) == len(survey.questions)
 
 
-def is_invalid_question_number(question_number):
-    return question_number not in range(len(survey.questions)) or \
-           question_number != len(responses)
+def get_question_number():
+    return len(responses)
+
+
+def is_this_last_question():
+    it_is = get_this_question_number() == len(survey.questions)
+    return it_is
+
+
+def get_this_question_number():
+    return get_question_number() + 1
